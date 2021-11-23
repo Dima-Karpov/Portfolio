07e71ebebe13ada7fa521b64baad9a25dd0c7990
Dima-Karpov <dimka.carpov2013@yandex.ru>
2021-11-23 23:02:26 +0300

    scss

--- src/header/nav/Nav.tsx
+++ src/header/nav/Nav.tsx
@@ -4,27 +4,26 @@ import { Link } from "react-scroll";
 import { AssembledMene } from './assembledMenu/AssembledMenu';
 
 export const Nav: React.FC = React.memo(() => {
-
     const items: string[] = ['home', 'skills', 'portfolio', 'contactMe'];
 
     return (
-        <div className={s.nav} >
-            <div className={s.container}>
-                {items.map((item, index) =>
+        <div className={ s.nav } >
+            <div className={ s.container }>
+                { items.map((item, index) =>
                     <Link
-                        key={index}
-                        className={s.link}
-                        activeClass={s.active}
-                        to={item}
-                        spy={true}
-                        smooth={true}
-                        offset={-99}
-                        duration={500}
+                        key={ index }
+                        className={ s.link }
+                        activeClass={ s.active }
+                        to={ item }
+                        spy={ true }
+                        smooth={ true }
+                        offset={ -99 }
+                        duration={ 500 }
                     >
-                        {item}
+                        { item }
                     </Link>
-                )}
-                <AssembledMene items={items} />
+                ) }
+                <AssembledMene items={ items } />
             </div>
         </div>
     )
